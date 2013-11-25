e03af14d993a18a6e64afb4899a59f3249a39651
Guilherme Moretti <guilherme.moretti@innvent.com.br>
2013-11-25 15:35:09 -0200

    Move range methods to a specific concern

--- lib/parsley_simple_form/constraints/basics/rangelength_constraint.rb
+++ lib/parsley_simple_form/constraints/basics/rangelength_constraint.rb
@@ -2,6 +2,7 @@ module ParsleySimpleForm
   module Constraints
     module Basics
       class RangeLengthConstraint < Constraints::BaseConstraint
+        include Concerns::RangeConcern
 
         def match?
           !@options[:rangelength].nil?
@@ -10,35 +11,10 @@ module ParsleySimpleForm
         def html_attributes
           range = parse_range(@options[:rangelength])
           {
-            :'data-rangelength' => range[:string],
+            :'parsley-rangelength' => range[:string],
             :'data-rangelength-message' => I18n::translate('form_validation.message.rangelength', min: range[:min], max: range[:max])
           }
         end
-
-        private
-        def parse_range(range)
-          { 
-            :string => range_to_string(range),
-            :min => min_range(range),
-            :max => max_range(range)
-          }
-        end
-
-        def range_to_string(range)
-          return "[#{range.min},#{range.max}]" if range.is_a?(Range)
-          range
-        end
-
-        def min_range(range)
-          return range.min if range.is_a?(Range)
-          range.match(/\[(\d),(\d)\]/)[1]
-        end
-
-        def max_range(range)
-          return range.max if range.is_a?(Range)
-          range.match(/\[(\d),(\d)\]/)[2]
-        end
-
       end
     end
   end
